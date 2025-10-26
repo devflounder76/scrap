0345c6f9f9583a03f405387823624d1a20d65ff3
DevFlounder76 <devflounder76@gmail.com>
2025-10-26 02:37:04 -0400

    Create CPP.cpp

---
+++ CPP.cpp
@@ -0,0 +1,8 @@
+/*
+Write a for loop that iterates nine times to find the maximum value in a list of nine integer values. Within the loop:
+
+Read an integer value from input into variable inVal.
+The first value assigned to inVal is the largest integer value read by default, since no other values have been read yet. Thus, if the current iteration is the first iteration, assign biggestVal with inVal.
+Otherwise, if inVal is greater than biggestVal, assign biggestVal with the value of inVal.
+Click here for example
+*/
